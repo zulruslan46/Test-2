177a061f2a270536a800a43034c2fb38f367c342
Ruslan <zulruslan46@gmail.com>
2024-04-11 16:29:42 +0200

    added 2 home_work_sql

--- qa_auto_tables.sql
+++ qa_auto_tables.sql
@@ -1,25 +1,25 @@
 create schema QA_AUTO;
 create table car_brands(
-id varchar(255),
+id int(255),
 title varchar(255)
 );
 create table car_models(
-id varchar(255),
+id int(255),
 car_Brandid varchar(255),
 title varchar(255)
 );
 create table users(
-id varchar(255),
+id int(255),
 first_name varchar(255),
 last_name varchar(255),
 email varchar(255),
 password varchar(255)
 );
 create table cars(
-id varchar(255),
-userit varchar(255),
-car_Brandid varchar(255),
-car_Modelid varchar(255),
-mileage varchar(255),
-initial_Mileage varchar(255)
+id int(255),
+userit int(255),
+car_Brandid int(255),
+car_Modelid int(255),
+mileage float(255),
+initial_Mileage float(255)
 );
